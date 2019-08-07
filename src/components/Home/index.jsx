d8dd2dcb3cd71a3dbb026719a555768c9f147b51
Bruno Cerecetto <bruno.cerecetto@gmail.com>
2019-08-06 22:42:21 -0300

    change style to flex

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -6,46 +6,25 @@ import ParticulesComponent from '../ParticulesComponent';
 import './index.scss';
 
 const HomeComponent = () => {
-  // eslint-disable-next-line no-undef
   const width = window.window.innerWidth;
-  // eslint-disable-next-line no-undef
   const height = window.window.innerHeight;
 
   return (
-    <div
-      style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        background: '#014972',
-      }}
-    >
+    <div className="background">
       <ParticulesComponent height={height} width={width} />
-      <div
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-        }}
-      >
-        <div className="title text unselectable">
-          <TextLoop className="unselectable" children={["Hello , I'm", 'Hola, Soy ', 'Oi, Sou ']} />
-          <span className="highlight unselectable"> Bruno Cerecetto</span>
-          .
-          <br />
-          <TextLoop
-            className="unselectable"
-            children={[
-              "I'm a full-stack web developer.",
-              'Soy desarrollador web Full Stack.',
-              'Eu sou um full-stack web developer.',
-            ]}
-          />
-        </div>
+      <div className="title text unselectable">
+        <TextLoop className="unselectable" children={["Hello , I'm", 'Hola, Soy ', 'Oi, Sou ']} />
+        <span className="highlight unselectable"> Bruno Cerecetto</span>
+        .
+        <br />
+        <TextLoop
+          className="unselectable"
+          children={[
+            "I'm a full-stack web developer.",
+            'Soy desarrollador web Full Stack.',
+            'Eu sou um full-stack web developer.',
+          ]}
+        />
       </div>
     </div>
   );
