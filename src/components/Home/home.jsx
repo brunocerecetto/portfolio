bde600013320c1559f4abce5b63ebd95c6a49844
Bruno Cerecetto <bruno.cerecetto@gmail.com>
2019-11-10 17:21:13 -0300

    home proptypes

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,17 +1,15 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable jsx-a11y/accessible-emoji */
-/* eslint-disable react/no-children-prop */
 import React from 'react';
 import TextLoop from 'react-text-loop';
+import PropTypes from 'prop-types';
 import ParticulesComponent from './particulesComponent';
 import styles from './home.module.scss';
 
-const HomeComponent = props => (
+const HomeComponent = ({ scrollTo }) => (
   <div className="w-full h-screen bg-background flex flex-col justify-between items-center">
     <div />
     <ParticulesComponent />
     <div className="text-white text-base md:text-xl lg:text-4xl leading-none md:leading-normal lg:leading-relaxed text-center">
-      <TextLoop children={["Hello , I'm", 'Hola, Soy ', 'Oi, Sou ']} />
+      <TextLoop children={["Hello , I'm", 'Hola, Soy ']} />
       <span className="text-highlight font-raleway-semibold font-bold"> Bruno Cerecetto</span>
         .
       <br />
@@ -19,17 +17,21 @@ const HomeComponent = props => (
         children={[
           "I'm a full-stack web developer.",
           'Soy desarrollador web Full Stack.',
-          'Eu sou um full-stack web developer.',
         ]}
       />
     </div>
     <div className="mb-4 z-50">
-      <button type="button" className={`text-white md:text-lg lg:text-2xl flex flex-col items-center ${styles.animate} ${styles.bounce}`} onClick={() => { props.scrollTo(); }}>
+      <button type="button" className={`text-white md:text-lg lg:text-2xl flex flex-col items-center ${styles.animate} ${styles.bounce}`} onClick={() => { scrollTo(); }}>
         <span>Find out more!</span>
         <i className="fas fa-chevron-down" />
       </button>
     </div>
   </div>
 );
+
+HomeComponent.propTypes = {
+  scrollTo: PropTypes.func.isRequired,
+};
+
 
 export default HomeComponent;
