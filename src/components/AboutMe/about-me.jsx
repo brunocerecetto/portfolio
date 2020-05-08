be2f00d32a1a348052dfae7dc0f82a59f5e9403f
Bruno Cerecetto <bruno.cerecetto@gmail.com>
2020-05-08 18:08:42 -0300

    :dizzy: added about me animations

--- src/components/AboutMe/about-me.jsx
+++ src/components/AboutMe/about-me.jsx
@@ -5,70 +5,84 @@ import Header from '../header/header';
 import ProgressBar from './progress-bar';
 import styles from './about-me.module.scss';
 
-const AboutMeComponent = () => (
-  <div className="min-h-screen">
-    <Header text="about" />
+const AboutMeComponent = () => {
+  const badges = [
+    { text: 'javascript', icon: 'fab fa-js-square' },
+    { text: 'angular', icon: 'fab fa-angular' },
+    { text: 'react', icon: 'fab fa-react' },
+    { text: 'Node JS', icon: 'fab fa-node-js' },
+  ];
 
-    <div className="md:mx-16 flex flex-col lg:flex-row md:justify-between">
-      <div className="flex flex-col justify-center items-center">
-        <div className="text-center mb-4">
-          <div className={styles.badge}>
-            <div className={styles.circle}>
-              <img className="rounded-full" src={me} alt="" />
-              <div className={styles.ribbon}>Who&apos;s this guy?</div>
+  const skills = [
+    { text: 'html', percentage: 90 },
+    { text: 'javascript', percentage: 90 },
+    { text: 'css', percentage: 65 },
+    { text: 'angular', percentage: 80 },
+    { text: '.net', percentage: 90 },
+    { text: 'sql', percentage: 70 },
+    { text: 'react', percentage: 65 },
+    { text: 'node.js', percentage: 45 },
+    { text: 'docker', percentage: 35 },
+  ];
+
+  return (
+    <div className="min-h-screen">
+      <Header text="about" />
+
+      <div className="md:mx-16 flex flex-col lg:flex-row md:justify-between">
+        <div className="flex flex-col justify-center items-center">
+          <div className="text-center mb-4">
+            <div className={styles.badge}>
+              <div className={styles.circle}>
+                <img className="rounded-full" src={me} alt="" />
+                <div className={styles.ribbon}>Who&apos;s this guy?</div>
+              </div>
             </div>
           </div>
+          <span className="text-center md:text-xl">
+            Bruno is a Full Stack Developer currently working at
+            <br />
+            <a
+              href="https://dominion-cs.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cursor-pointer text-highlight"
+            >
+              Dominion Capital Strategies
+            </a>
+            {' '}
+            in Montevideo, Uruguay 🇺🇾
+          </span>
+          <span className="text-center md:text-xl">
+            His role in Dominion is to make sure that everything
+            <br />
+            works as it should on the Front-End
+            <br />
+            but does things on the backend too 😂
+            <br />
+            Javascript is his passion. ❤️
+          </span>
         </div>
-        <span className="text-center md:text-xl">
-          Bruno is a Full Stack Developer currently working at
-          <br />
-          <a
-            href="https://dominion-cs.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="cursor-pointer text-highlight"
-          >
-            Dominion Capital Strategies
-          </a>
-          {' '}
-          in Montevideo, Uruguay 🇺🇾
-        </span>
-        <span className="text-center md:text-xl">
-          His role in Dominion is to make sure that everything
-          <br />
-          works as it should on the Front-End
-          <br />
-          but does things on the backend too 😂
-          <br />
-          Javascript is his passion. ❤️
-        </span>
-      </div>
-      <div className="hidden lg:block -ml-40">
-        <hr className={`border h-ninety ${styles.border_solid} bg-highlight text-highlight`} />
-      </div>
-      <div className="flex flex-col items-center mt-5 lg:mt-0">
-        <h2 className="text-2xl font-black mb-5">Technologies I enjoy:</h2>
-        <div className="flex flex-row justify-between">
-          <Badge text="Javascript" icon="fab fa-js-square" />
-          <Badge text="Angular" icon="fab fa-angular" />
-          <Badge text="React" icon="fab fa-react" />
-          <Badge text="Node JS" icon="fab fa-node-js" />
+        <div className="hidden lg:block -ml-40">
+          <hr className={`border h-ninety ${styles.border_solid} bg-highlight text-highlight`} />
         </div>
-        <h2 className="text-2xl font-black my-5">Skills:</h2>
-        <div className="flex flex-col items-center w-full mb-20">
-          <ProgressBar skill="html" width="1/12" />
-          <ProgressBar skill="angular" width="1/12" />
-          <ProgressBar skill="javascript" width="2/12" />
-          <ProgressBar skill=".net" width="1/4" />
-          <ProgressBar skill="css" width="1/4" />
-          <ProgressBar skill="sql" width="2/5" />
-          <ProgressBar skill="react" width="2/5" />
-          <ProgressBar skill="node.js" width="2/5" />
-          <ProgressBar skill="docker" width="8/12" />
+        <div className="flex flex-col items-center mt-5 lg:mt-0">
+          <h2 className="text-2xl font-black mb-5">Technologies I enjoy:</h2>
+          <div className="flex flex-row justify-between">
+            {badges.map(b => (
+              <Badge key={b.text} text={b.text} icon={b.icon} />
+            ))}
+          </div>
+          <h2 className="text-2xl font-black my-5">Skills:</h2>
+          <div className="flex flex-col items-center w-full mb-20">
+            {skills.map(s => (
+              <ProgressBar key={s.text} skill={s.text} percentage={s.percentage} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default AboutMeComponent;
--- src/components/AboutMe/progress-bar/progress-bar.jsx
+++ src/components/AboutMe/progress-bar/progress-bar.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSpring, animated, config } from 'react-spring';
+import VisibilitySensor from 'react-visibility-sensor';
 
-const ProgressBar = ({ skill, width }) => (
-  <div className="flex flex-row justify-between w-10/12 md:w-full bg-highlight rounded-lg my-2 h-6">
-    <div className="text-white">
-      <span className="mx-2 items-center text-xs">
-        {skill}
-      </span>
-    </div>
-    <div className={`bg-gray-500 flex justify-end w-${width} rounded-r-lg h-6`}>
-      <span className="mr-3 items-center items-center text-xs">
-        {' '}
-      </span>
-    </div>
-  </div>
-);
+const ProgressBar = ({ skill, percentage }) => {
+  const [showBar, setIsInView] = React.useState(false);
+  const onVisibilityChange = isInView => setIsInView(isInView);
+
+  const progress = useSpring({
+    width: showBar ? `${percentage}%` : '0%',
+    config: config.molasses,
+  });
+
+  return (
+    <VisibilitySensor onChange={onVisibilityChange}>
+      <div className="flex w-10/12 md:w-full bg-gray-500 rounded-lg my-2 h-6 text-white">
+        <animated.div className="bg-highlight rounded-l-inherit" style={progress}>
+          <span className="mx-2 items-center text-xs">{skill}</span>
+        </animated.div>
+      </div>
+    </VisibilitySensor>
+  );
+};
 
 ProgressBar.propTypes = {
   skill: PropTypes.string.isRequired,
-  width: PropTypes.string.isRequired,
+  percentage: PropTypes.number.isRequired,
 };
 
 export default ProgressBar;
